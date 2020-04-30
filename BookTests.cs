29c870f9a86f22e3e022511b55d22308486b3904
Nolanmorley <59670812+Nolanmorley@users.noreply.github.com>
2020-04-30 14:39:57 -0400

    Create BookTests.cs

---
+++ BookTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+
+namespace Fisher.Bookstore.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void AddBookPrice()
+        {
+            var book = new Book()
+            {
+                Id = 1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+
+            //Act
+            book.Price = 7.99;
+
+            //Assert
+            Assert.Equal(7.99, book.Price);
+        }
+        [Fact]
+        public void TestMemberDiscount()
+        {
+           var book = new Book()
+            {
+                Id = 1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+
+            //Act
+            book.ApplyMemberDiscount();
+
+            //Assert
+            var discountPrice = 9.99 * .9;
+            AssemblyLoadEventArgs.Equals(discountPrice, book.Price);
+            
+        }
+    }
+}
